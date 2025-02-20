a680885dfa385145c1aa0129f11c341c8d67f1bf
Jonas van den Berg <jonas@vandenbe.com>
2025-02-20 22:01:07 +0100

    Add lifetime tracking during debugging
    This helps with tracking down usage errors more easily during development.
    This does not cause any additional overhead in production builds.

--- include/ungive/utility/live_value.h
+++ include/ungive/utility/live_value.h
@@ -9,6 +9,18 @@
 #include <stdexcept>
 #include <utility>
 
+// Track get() lifetimes during debugging.
+#if !defined(NDEBUG) && !defined(NO_TRACK_LIFETIMES)
+#define TRACK_LIFETIMES
+#include <set>
+#endif
+
+// Enable lifetime recording during unit tests.
+#if defined(TRACK_LIFETIMES) && defined(UNGIVE_UTILITY_TEST)
+#define LIFETIME_RECORDING
+#include <deque>
+#endif
+
 namespace ungive::utility
 {
 
@@ -25,6 +37,14 @@ namespace ungive::utility
 template <typename T>
 class LiveValue
 {
+private:
+    LiveValue(std::shared_ptr<T>&& ptr) : m_value{ std::move(ptr) }
+    {
+#ifdef TRACK_LIFETIMES
+        m_lifetime_thread = std::thread(&LiveValue::track_lifetimes, this);
+#endif
+    }
+
 public:
     using value_type = T;
 
@@ -32,15 +52,15 @@ public:
     static constexpr auto default_get_lifetime =
         std::chrono::milliseconds{ 100 };
 
-    LiveValue() : m_value{ std::make_shared<T>() } {}
+    LiveValue() : LiveValue(std::make_shared<T>()) {}
 
-    LiveValue(T&& value) : m_value{ std::make_shared<T>(std::move(value)) } {}
+    LiveValue(T&& value) : LiveValue(std::make_shared<T>(std::move(value))) {}
 
-    LiveValue(T const& value) : m_value{ std::make_shared<T>(value) } {}
+    LiveValue(T const& value) : LiveValue(std::make_shared<T>(value)) {}
 
     template <typename... Args>
     LiveValue(Args&&... args)
-        : m_value{ std::make_shared<T>(std::forward<Args>(args)...) }
+        : LiveValue(std::make_shared<T>(std::forward<Args>(args)...))
     {
     }
 
@@ -54,6 +74,14 @@ public:
 
         const std::lock_guard lock(m_mutex);
         assert(m_refs == 0 && "dangling get references exist");
+
+#ifdef TRACK_LIFETIMES
+        stop_lifetime_thread();
+#ifdef LIFETIME_RECORDING
+        // There should be no errors left in the history.
+        assert(m_lifetime_history.empty());
+#endif // LIFETIME_RECORDING
+#endif // TRACK_LIFETIMES
     }
 
     /**
@@ -80,11 +108,6 @@ public:
     std::shared_ptr<T> get(
         std::chrono::milliseconds lifetime = LiveValue::default_get_lifetime)
     {
-        // TODO during debugging/development:
-        // track that each reference is destructed before its lifetime ends.
-        // spin up another thread that checks this. that should obviously
-        // not be done during production (too much overhead).
-
         if (lifetime <= std::chrono::milliseconds::zero()) {
             throw std::invalid_argument(
                 "the lifetime must be greater than zero");
@@ -92,10 +115,23 @@ public:
 
         const std::lock_guard lock(m_mutex);
         m_refs += 1;
-        std::shared_ptr<T> result(
-            m_value.get(), std::bind(&LiveValue<T>::get_dtor, this));
-        m_set_wait =
-            std::max(m_set_wait, std::chrono::steady_clock::now() + lifetime);
+        auto timepoint = std::chrono::steady_clock::now() + lifetime;
+
+#ifdef TRACK_LIFETIMES
+        auto destructor =
+            std::bind(&LiveValue<T>::get_dtor_tracking, this, timepoint);
+        {
+            const std::lock_guard lock(m_lifetime_mutex);
+            m_lifetime_expirations.insert(timepoint);
+            m_lifetime_update = true;
+            m_lifetime_cv.notify_all();
+        }
+#else
+        auto destructor = std::bind(&LiveValue<T>::get_dtor, this);
+#endif // TRACK_LIFETIMES
+
+        std::shared_ptr<T> result(m_value.get(), destructor);
+        m_set_wait = std::max(m_set_wait, timepoint);
         return result;
     }
 
@@ -190,7 +226,7 @@ private:
     }
 
     std::mutex m_mutex{};
-    std::condition_variable m_cv;
+    std::condition_variable m_cv{};
     std::atomic<size_t> m_refs{ 0 };
     std::chrono::steady_clock::time_point m_set_wait{
         std::chrono::steady_clock::time_point::min()
@@ -199,6 +235,142 @@ private:
     // A pointer to the stored value. The pointer is never modified or replaced.
     // The value pointed to by the pointer may be be modified though.
     const std::shared_ptr<T> m_value{};
+
+#ifdef TRACK_LIFETIMES
+
+#ifdef LIFETIME_RECORDING
+private:
+    bool m_lifetime_record_history{ false };
+    std::condition_variable m_lifetime_history_cv{};
+    std::deque<std::pair<std::chrono::milliseconds, bool>> m_lifetime_history;
+
+public:
+    inline void _stop_lifetime_tracking() { stop_lifetime_thread(); }
+
+    // Records lifetime errors instead of causing assertion errors.
+    void _record_lifetime_history()
+    {
+        const std::lock_guard lock(m_lifetime_mutex);
+        m_lifetime_record_history = true;
+    }
+
+    // Pops the last lifetime error in the queue.
+    inline decltype(m_lifetime_history)::value_type
+    _await_lifetime_history_entry()
+    {
+        std::unique_lock lock(m_lifetime_mutex);
+        m_lifetime_history_cv.wait_for(lock, std::chrono::seconds{ 1 }, [this] {
+            return !m_lifetime_history.empty();
+        });
+        assert(!m_lifetime_history.empty());
+        auto front = m_lifetime_history.front();
+        m_lifetime_history.pop_front();
+        return front;
+    }
+#endif // LIFETIME_RECORDING
+
+private:
+    std::thread m_lifetime_thread{};
+    std::mutex m_lifetime_mutex{};
+    std::condition_variable m_lifetime_cv{};
+    std::multiset<std::chrono::steady_clock::time_point>
+        m_lifetime_expirations{};
+    bool m_lifetime_update{ false };
+    bool m_lifetime_stop{ false };
+
+    void track_lifetimes()
+    {
+        std::unique_lock lock(m_lifetime_mutex);
+        while (!m_lifetime_stop) {
+            auto timepoint = std::chrono::steady_clock::time_point::max();
+            if (!m_lifetime_expirations.empty()) {
+                timepoint = *m_lifetime_expirations.begin();
+            }
+
+#ifdef LIFETIME_RECORDING
+            auto start = std::chrono::steady_clock::now();
+#endif // LIFETIME_RECORDING
+
+            m_lifetime_cv.wait_until(lock, timepoint, [this] {
+                return m_lifetime_update || m_lifetime_stop;
+            });
+            if (m_lifetime_stop) {
+                break;
+            }
+            if (m_lifetime_update) {
+                m_lifetime_update = false;
+#ifdef LIFETIME_RECORDING
+                if (m_lifetime_record_history) {
+                    m_lifetime_history.push_back(std::make_pair(
+                        std::chrono::duration_cast<std::chrono::milliseconds>(
+                            std::chrono::steady_clock::now() - start),
+                        false));
+                    m_lifetime_history_cv.notify_all();
+                }
+#endif // LIFETIME_RECORDING
+                continue;
+            }
+
+            assert(!m_lifetime_expirations.empty());
+            assert(timepoint == *m_lifetime_expirations.begin());
+            assert(std::chrono::steady_clock::now() >= timepoint);
+
+            m_lifetime_expirations.erase(m_lifetime_expirations.begin());
+
+            bool assertion_error = true;
+#ifdef LIFETIME_RECORDING
+            if (m_lifetime_record_history) {
+                m_lifetime_history.push_back(std::make_pair(
+                    std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::steady_clock::now() - start),
+                    true));
+                m_lifetime_history_cv.notify_all();
+                assertion_error = false;
+            }
+#endif // LIFETIME_RECORDING
+
+            if (assertion_error) {
+                // A get key is being used beyond its lifetime.
+                assert(false && "a reference is used beyond its lifetime");
+            }
+        }
+    }
+
+    void get_dtor_tracking(std::chrono::steady_clock::time_point timepoint)
+    {
+        {
+            const std::lock_guard lock(m_lifetime_mutex);
+            auto it = m_lifetime_expirations.find(timepoint);
+            if (it != m_lifetime_expirations.end()) {
+                // Only erase one item with this value, not all.
+                m_lifetime_expirations.erase(it);
+            }
+            m_lifetime_update = true;
+        }
+        get_dtor();
+    }
+
+    void stop_lifetime_thread()
+    {
+        {
+            const std::lock_guard lock(m_lifetime_mutex);
+            if (m_lifetime_stop) {
+                return;
+            }
+            m_lifetime_stop = true;
+            m_lifetime_cv.notify_all();
+        }
+        m_lifetime_thread.join();
+    }
+#endif // TRACK_LIFETIMES
 };
 
 } // namespace ungive::utility
+
+#ifdef TRACK_LIFETIMES
+#undef TRACK_LIFETIMES
+#endif
+
+#ifdef LIFETIME_RECORDING
+#undef LIFETIME_RECORDING
+#endif
